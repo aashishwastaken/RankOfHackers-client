9c6a8257e027f88566155e49d096927c6cee33bd
Aashish Prasad <aashishprasad45@gmail.com>
2021-04-24 18:18:53 +0530

    Images uploaded

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
         return ( 
             <header className="header">
                 <Link to="/">
-                    {/* <img src="" style={{width:'2em',height:'2em'}}> */}
+                    <img alt="background" src="https://raw.githubusercontent.com/aashishwastaken/RankOfHackers-client/4340e639ae34a517effdb143ec74d48f37f2c954/public/favicon.svg" style={{width:'2em',height:'2em'}} />
                     <h1 className="title">Rank Of Hackers</h1></Link>
                 <AuthOptions />
             </header>
